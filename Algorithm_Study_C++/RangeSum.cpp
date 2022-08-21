b68b79c39a1f7a6841c56beb4438cb49623fa691
SGGLYJ <lg250712@gmail.com>
2022-08-21 18:27:00 +0900

    Test

---
+++ Algorithm_Study_C++/RangeSum.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) 
+{
+	
+	return 0;
+}
+
+/// <summary>
+/// 주어진 벡터의 부분합을 구하는 함수 작성하기
+/// </summary>
+/// <param name="data"></param>
+/// <param name="index"></param>
+/// <returns></returns>
+int PartialSum(const vector<int>& data, int index) 
+{
+
+}
+
+/// <summary>
+/// 주어진 벡터의 부분합을 모두 구하여 벡터로 반환하는 함수 작성하기
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+vector<int> GetAllPartialSum(const vector<int>& data) 
+{
+	vector<int> result(data.size());
+	result[0] = data[0];
+
+
+
+}
+
+/// <summary>
+/// 주어진 벡터의 구간합을 구하는 함수 작성하기
+/// </summary>
+/// <param name="data"></param>
+/// <param name="start"></param>
+/// <param name="end"></param>
+/// <returns></returns>
+int RangeSum(const vector<int>& data, int start, int end) 
+{
+
+}
+
+/// <summary>
+/// 주어진 벡터에서 구간합이 특정 값이 되는 구간의 개수 찾기
+/// </summary>
+/// <param name="data"></param>
+/// <param name="matchedValue"></param>
+/// <returns></returns>
+int CountAllMatchedRangeSum(const vector<int>& data, int matchedValue)
+{
+
+}
